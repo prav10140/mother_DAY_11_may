82908dcf36acb9a8b03b814251d54a69646728cb
PRAVEEN PATEL <prav9406@gmail.com>
2025-04-30 23:10:24 +0530

    Update DestinationsList.js

--- src/components/DestinationsList.js
+++ src/components/DestinationsList.js
@@ -1,17 +1,17 @@
-import { Link } from "react-router-dom";
-import "./DestinationsList.css";
+import { Link } from "react-router-dom"
+import "./DestinationsList.css"
 
 const DestinationsList = () => {
   const categories = [
-    { name: "Mother's Day Tributes", slug: "mothers-day-tributes" },
-    { name: "Lessons from Moms", slug: "lessons-from-moms" },
-    { name: "Personal Stories", slug: "personal-stories" },
-    { name: "Messages of Love", slug: "messages-of-love" },
-  ];
+    { name: "First-Time Moms", slug: "first-time-moms" },
+    { name: "Grandmothers", slug: "grandmothers" },
+    { name: "Single Moms", slug: "single-moms" },
+    { name: "Mother Figures", slug: "mother-figures" },
+  ]
 
   return (
     <div className="destinations-list">
-      <h3 className="destinations-title">Categories</h3>
+      <h3 className="destinations-title">Popular Categories</h3>
       <ul>
         {categories.map((category, index) => (
           <li key={index}>
@@ -20,10 +20,10 @@ const DestinationsList = () => {
         ))}
       </ul>
       <Link to="/categories" className="read-more-btn">
-        Read more
+        View All
       </Link>
     </div>
-  );
-};
+  )
+}
 
-export default DestinationsList;
+export default DestinationsList
