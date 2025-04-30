ee03dcf1bc221e987c033d857e49be7ce5f9f800
PRAVEEN PATEL <prav9406@gmail.com>
2025-04-30 17:48:21 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import HomePage from "./pages/HomePage"
+import BlogPost from "./pages/BlogPost"
+import CategoryPage from "./pages/CategoryPage"
+import AboutPage from "./pages/AboutPage"
+import "./App.css"
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/post/:slug" element={<BlogPost />} />
+            <Route path="/category/:category" element={<CategoryPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
