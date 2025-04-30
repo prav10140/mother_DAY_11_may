f315466595eca89aeeb656504418d4750b89cf58
PRAVEEN PATEL <prav9406@gmail.com>
2025-04-30 17:50:15 +0530

    Create BlogPostCard.js

---
+++ src/components/BlogPostCard.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom"
+import "./BlogPostCard.css"
+
+const BlogPostCard = ({ post }) => {
+  return (
+    <article className="blog-post-card">
+      <div className="post-image">
+        <Link to={`/post/${post.slug}`}>
+          <img src={post.image || "/placeholder.svg"} alt={post.title} />
+        </Link>
+      </div>
+      <div className="post-content">
+        <div className="post-meta">
+          <Link to={`/category/${post.category.toLowerCase()}`} className="post-category">
+            #{post.category}
+          </Link>
+          <span className="post-reading-time">{post.readingTime} reading</span>
+        </div>
+        <h3 className="post-title">
+          <Link to={`/post/${post.slug}`}>{post.title}</Link>
+        </h3>
+        <p className="post-excerpt">{post.excerpt}</p>
+        <Link to={`/post/${post.slug}`} className="read-more-btn">
+          Read more
+        </Link>
+      </div>
+    </article>
+  )
+}
+
+export default BlogPostCard
