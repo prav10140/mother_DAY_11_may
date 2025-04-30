1fc1c5e4772dc7eb5b13afa02db2ac8edc7aa6be
PRAVEEN PATEL <prav9406@gmail.com>
2025-04-30 21:56:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import HomePage from "./pages/HomePage"
-import BlogPost from "./pages/BlogPost"
-import CategoryPage from "./pages/CategoryPage"
-import AboutPage from "./pages/AboutPage"
-import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import BlogPost from "./pages/BlogPost";
+import CategoryPage from "./pages/CategoryPage"; // Added CategoryPage
+import AboutPage from "./pages/AboutPage";
+import "./App.css";
 
 function App() {
   return (
@@ -16,14 +16,14 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/post/:slug" element={<BlogPost />} />
-            <Route path="/category/:category" element={<CategoryPage />} />
+            <Route path="/category/:category/page/:pageNumber" element={<CategoryPage />} /> {/* Pagination added */}
             <Route path="/about" element={<AboutPage />} />
           </Routes>
         </main>
         <Footer />
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
