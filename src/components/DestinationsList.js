c6ae87aa42e64a25869ce1a9d6a151efcd428c2a
PRAVEEN PATEL <prav9406@gmail.com>
2025-04-30 21:33:12 +0530

    Update DestinationsList.js

--- src/components/DestinationsList.js
+++ src/components/DestinationsList.js
@@ -1,29 +1,29 @@
-import { Link } from "react-router-dom"
-import "./DestinationsList.css"
+import { Link } from "react-router-dom";
+import "./DestinationsList.css";
 
 const DestinationsList = () => {
-  const destinations = [
-    { name: "Tokyo", slug: "tokyo" },
-    { name: "Rome", slug: "rome" },
-    { name: "San Francisco", slug: "san-francisco" },
-    { name: "San Jose", slug: "san-jose" },
-  ]
+  const categories = [
+    { name: "Mother's Day Tributes", slug: "mothers-day-tributes" },
+    { name: "Lessons from Moms", slug: "lessons-from-moms" },
+    { name: "Personal Stories", slug: "personal-stories" },
+    { name: "Messages of Love", slug: "messages-of-love" },
+  ];
 
   return (
     <div className="destinations-list">
-      <h3 className="destinations-title">Destinations</h3>
+      <h3 className="destinations-title">Categories</h3>
       <ul>
-        {destinations.map((destination, index) => (
+        {categories.map((category, index) => (
           <li key={index}>
-            <Link to={`/category/${destination.slug}`}>{destination.name}</Link>
+            <Link to={`/category/${category.slug}`}>{category.name}</Link>
           </li>
         ))}
       </ul>
-      <Link to="/destinations" className="read-more-btn">
+      <Link to="/categories" className="read-more-btn">
         Read more
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default DestinationsList
+export default DestinationsList;
